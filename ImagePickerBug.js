528374d7232875a7306fd4834fb83420afd311e7
BugHunter-X <askzenai@gmail.com>
2025-02-03 21:28:09 +0100

    Add ImagePickerBug.js file

---
+++ ImagePickerBug.js
@@ -0,0 +1,1 @@
+This bug occurs when using the Expo ImagePicker library and selecting multiple images.  The selected images are not consistently returned in the same order as they were selected by the user. This inconsistency can lead to unexpected behavior in applications that rely on the order of selected images, such as image carousels or sequential image processing workflows.
